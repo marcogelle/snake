16d346b7c57de86cf2d091921d233beb96a4382b
Marco Gellecanao <marcogelle@berkeley.edu>
2020-08-29 18:05:25 -0700

    border game over; got rid of global vars

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from typing import Tuple
 
 SCRN_HEIGHT = 600
 SCRN_WIDTH = 600
@@ -54,27 +55,49 @@ class Snake:
     def move(self, dx, dy) -> None:
         head = self.parts[0]
         trail_x, trail_y = head.x, head.y
-
         head.move(dx, dy)
-        if (head.x < 0 or head.x >= GRID_WIDTH or
-            head.y < 0 or head.y >= GRID_HEIGHT):
-            run = False
-
         for part in self.parts[1:]:
             dx, dy = trail_x - part.x, trail_y - part.y
             trail_x, trail_y = part.x, part.y
             part.move(dx, dy)
 
+    def grow(self) -> None:
+        tail = self.parts[-1]
+        self.parts.append(SnakePart(tail.x, tail.y))
+
+    def head_x(self) -> int:
+        return self.parts[0].x
+
+    def head_y(self) -> int:
+        return self.parts[0].y
+
+def update_snake_dir(keys: Tuple[bool, ...], dx: int, dy: int) -> Tuple[int, int]:
+    if keys[pygame.K_UP]:
+        dx, dy = 0, -1
+    if keys[pygame.K_DOWN]:
+        dx, dy = 0, 1
+    if keys[pygame.K_LEFT]:
+        dx, dy = -1, 0
+    if keys[pygame.K_RIGHT]:
+        dx, dy = 1, 0
+    return dx, dy
+
+def checkFood() -> None:
+    if food.x == snake.head_x() and food.y == snake.head_y():
+        snake.grow()
+        food.spawn()
+
 def redrawGameWindow() -> None:
     screen.fill(BG_COLOR)
     snake.draw()
     food.draw()
-
     pygame.display.update()
 
 snake = Snake(random.randrange(0, GRID_WIDTH), random.randrange(0, GRID_HEIGHT))
 food = Food(random.randrange(0, GRID_WIDTH), random.randrange(0, GRID_HEIGHT))
 dx, dy = 0, 0
+
+# mainloop
 run = True
 while run:
     clock.tick(FPS)
@@ -85,16 +108,14 @@ while run:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
         run = False
-    if keys[pygame.K_UP]:
-        dx, dy = 0, -1
-    if keys[pygame.K_DOWN]:
-        dx, dy = 0, 1
-    if keys[pygame.K_LEFT]:
-        dx, dy = -1, 0
-    if keys[pygame.K_RIGHT]:
-        dx, dy = 1, 0
-    snake.move(dx, dy)
 
+    dx, dy = update_snake_dir(keys, dx, dy)
+    snake.move(dx, dy)
+    if (snake.head_x() < 0 or snake.head_x() >= GRID_WIDTH or
+        snake.head_y() < 0 or snake.head_y() >= GRID_HEIGHT):
+        run = False
+
+    checkFood()
     redrawGameWindow()
 
 pygame.quit()
