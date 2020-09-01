79a5b21fcaa4d80ed0f2f86dd00b884e38ef0587
Marco Gellecanao <marcogelle@berkeley.edu>
2020-09-01 00:26:27 -0700

    attempting to change neat parameters

--- neatsnake.py
+++ neatsnake.py
@@ -6,19 +6,64 @@ import neat
 import playsnake
 
 def get_inputs(snake, food):
-    return [1] * 24 # dummy function, will replace later
+    x, y = snake.head_x(), snake.head_y()
+    inputs = []
 
-def orient_neat_snake(net, snake, food):
-    # dummy function, will replace later
-    snake.dx = 1
-    snake.dy = 0
+    # Distance to food
+    inputs.append(food.x - x)
+    inputs.append(food.y - y)
+
+    # Distances to walls
+    inputs.append(y)
+    inputs.append(playsnake.GRID_HEIGHT - y)
+    inputs.append(x)
+    inputs.append(playsnake.GRID_WIDTH - x)
+
+    # Distances to snake parts. -1 if no snake part in that direction.
+    pos = snake.positions()
+    for dx, dy in ((0, -1), (0, 1), (-1, 0), (1, 0), (1, -1), (1, 1), (-1, 1), (-1, -1)):
+        x1, y1 = x, y
+        i = 0
+        found = False
+        while not playsnake.outside(x1, y1):
+            x1 += dx
+            y1 += dy
+            i += 1
+            if (x1, y1) in pos:
+                inputs.append(i)
+                found = True
+                break
+        if not found:
+            inputs.append(0)
+
+    return inputs
+
+def orient_neat_snake(snake, net, inputs):
+    output = net.activate(inputs)
+
+    max_i, max_out = 0, output[0]
+    for i, o in enumerate(output[1:], 1):
+        if o > max_out:
+            max_i = i
+
+    if max_i == 0:
+        snake.dx, snake.dy = 0, -1
+    elif max_i == 1:
+        snake.dx, snake.dy = 0, 1
+    elif max_i == 2:
+        snake.dx, snake.dy = -1, 0
+    elif max_i == 2:
+        snake.dx, snake.dy = 1, 0
+    else:
+        assert RuntimeError("The neural net has more than 4 outputs????")
 
 def eval_genomes(genomes, config):
     pygame.init()
     screen = pygame.display.set_mode((playsnake.SCRN_WIDTH,
         playsnake.SCRN_HEIGHT))
-    pygame.display.set_caption("Snake from First Genome of this Population")
+    pygame.display.set_caption("First Individual of this Population")
     clock = pygame.time.Clock()
+    # screen = None
 
     nets = []
     snakes = []
@@ -38,6 +83,8 @@ def eval_genomes(genomes, config):
         food_list.append(food)
         gen_list.append(genome)
 
+    no_food = [0] * len(snakes)
+    first = True
     run = True
     while run and snakes:
         clock.tick(playsnake.FPS)
@@ -51,7 +98,7 @@ def eval_genomes(genomes, config):
 
         for net, snake, food, genome in zip(nets, snakes, food_list, gen_list):
             inputs = get_inputs(snake, food)
-            orient_neat_snake(net, snake, food)
+            orient_neat_snake(snake, net, inputs)
             genome.fitness += 0.1
             snake.move()
 
@@ -62,12 +109,31 @@ def eval_genomes(genomes, config):
                 snakes.pop(i)
                 food_list.pop(i)
                 gen_list.pop(i)
+                no_food.pop(i)
+                if i == 0:
+                    first = False
 
-        for snake, food, genome in zip(snakes, food_list, gen_list):
+        for i, (snake, food, genome) in enumerate(zip(snakes, food_list, gen_list)):
             if playsnake.update_food(snake, food):
                 genome.fitness += 10
+                no_food[i] = 0
+            else:
+                no_food[i] += 1
 
-        if snakes:
+        for i, nf in enumerate(no_food):
+            if nf >= 100:
+                gen_list[i].fitness -= 100
+        for i, nf in enumerate(no_food):
+            if nf >= 15:
+                nets.pop(i)
+                snakes.pop(i)
+                food_list.pop(i)
+                gen_list.pop(i)
+                no_food.pop(i)
+                if i == 0:
+                    first = False
+
+        if first:
             playsnake.redraw_screen(screen, snakes[0], food_list[0])
 
     pygame.quit()
@@ -85,7 +151,7 @@ def run(config_file):
     p.add_reporter(stats)
 
     # Run for up to 50 generations.
-    winner = p.run(eval_genomes, 50)
+    winner = p.run(eval_genomes, 300)
 
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
